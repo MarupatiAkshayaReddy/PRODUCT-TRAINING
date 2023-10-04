ace3ee1446142690d44cb6a07177f31542de328a
MarupatiAkshayaReddy <146242013+MarupatiAkshayaReddy@users.noreply.github.com>
2023-10-04 11:30:34 +0530

    Add files via upload

---
+++ 28-09-2023/datatyperange.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    long int mobile=9999999999;
+    printf("%ld",mobile);
+    short age=40;
+    printf("\n %hi",age);
+    int pincode=123456;
+    printf("\n%d",pincode);
+    long int adhar=123456789012;
+    printf("\n%ld",adhar);
+    long long int creditcard=1234567890123456;
+    printf("\n%lld",creditcard);
+
+    return 0;
+}
