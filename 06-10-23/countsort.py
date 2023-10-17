7d3b127cdb1de632494cb65e11bb0ab6a06e1108
MarupatiAkshayaReddy <146242013+MarupatiAkshayaReddy@users.noreply.github.com>
2023-10-17 09:14:31 +0530

    Add files via upload

---
+++ 06-10-23/countsort.py
@@ -0,0 +1,16 @@
+l=[2,4,8,3,8,1,2,4,7,4]
+count=[0 for i in range(10)]
+
+for i in range(len(l)):
+    count[l[i]]+=1
+print(count)
+
+for i in range(1,len(count)):
+    count[i]+=count[i-1]
+print(count)
+
+res=[0]*len(l)
+for i in range(len(l)):
+    res[count[l[i]]-1]=l[i]
+    count[l[i]]-=1
+print(res)
---
+++ 06-10-23/sumdivideandconquerapproach.py
@@ -0,0 +1,11 @@
+#finding sum of array elements using divide and conquer approach
+def sum(l,si,ei):
+    if si==ei:
+        return l[si]
+    if si>ei:
+        return -1
+    mid=(si+ei)//2
+    return sum(l,si,mid)+sum(l,mid+1,ei)
+
+l=list(map(int,input().split()))
+print(sum(l,0,len(l)-1))
